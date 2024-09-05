346e8b7de4c950be5a9eee9240bb701d46daec20
ThinLiquid <thnlqd@gmail.com>
2024-09-05 11:59:55 +0100

    Update emojis.js

--- emojis.js
+++ emojis.js
@@ -3,5 +3,15 @@
     name: 'OMG',
     shortcodes: ['omg'],
     url: 'https://cdn3.emoji.gg/emojis/2028-omg.png',
+  },
+  {
+    name: 'blaa',
+    shortcodes: ['blaa'],
+    url: 'https://cdn3.emoji.gg/emojis/4325-blaa.png'
+  },
+  {
+    name: 'amogus',
+    shortcodes: ['amogus'],
+    url: 'https://cdn3.emoji.gg/emojis/4373-amongus-dancing.gif'
   }
 ]
